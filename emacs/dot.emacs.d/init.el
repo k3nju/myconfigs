65ed972ab990743241c490c82b1e6fffd5349f1d
kenju <priv.kenju@gmail.com>
2015-08-18 12:20:31 +0900

    add org

--- emacs/dot.emacs.d/init.el
+++ emacs/dot.emacs.d/init.el
@@ -18,8 +18,9 @@
   
   ;; package
   (when (require 'package nil t)
-	(add-to-list 'package-archives '("melpa"		.	"http://melpa.org/packages/"))
-	(add-to-list 'package-archives '("marmalade"	.	"http://marmalade-repo.org/packages/"))
+	(add-to-list 'package-archives '("melpa"	 . "http://melpa.org/packages/") t)
+	(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
+	(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 	(package-initialize))
 
   ;; auto-complete
