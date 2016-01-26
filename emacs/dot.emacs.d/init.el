da58101ca05acbeb297de0dfc10ea71f2108e1d4
kenju <priv.kenju@gmail.com>
2016-01-26 19:50:45 +0900

    fix typo

--- emacs/dot.emacs.d/init.el
+++ emacs/dot.emacs.d/init.el
@@ -180,7 +180,7 @@
 (when (require 'ggtags nil t)
   (add-hook 'c-mode-common-hook
 			(lambda ()
-			  (when (derived-mode-p 'c-mode 'c++mode 'java-mode)
+			  (when (derived-mode-p 'c-mode 'c++-mode 'java-mode)
 				(ggtags-mode 1)))))
 
 ;; Open .h .rl files in C++ mode
