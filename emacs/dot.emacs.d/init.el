c1291a0e754b668b1ded8993aa48323ac667fbc4
kenju <kenju.takano+git@gmail.com>
2017-04-26 00:05:52 +0900

    after 16,7 years of starting coding, i changed 4 to 2.

--- emacs/dot.emacs.d/init.el
+++ emacs/dot.emacs.d/init.el
@@ -284,7 +284,7 @@
  '(tab-stop-list
    (quote
 	(4 8 12 16 20 24 28 32 36 40 44 48 52 56 60 64 68 72 76 80 84 88 92 96 100 104 108 112 116 120)))
- '(tab-width 4)
+ '(tab-width 2)
  '(tool-bar-mode nil)
  '(tooltip-mode nil)
  '(truncate-lines t)
