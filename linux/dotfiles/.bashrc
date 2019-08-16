33435a2b721fc20ec8cb467ff4ee4336d079f606
kenju <kenjutakano+git@gmail.com>
2019-08-16 21:43:26 +0900

    a

--- linux/dotfiles/.bashrc
+++ linux/dotfiles/.bashrc
@@ -27,3 +27,7 @@ function wttr {
 
 
 
+export http_proxy=''
+export https_proxy=''
+export ftp_proxy=''
+export socks_proxy=''
--- linux/dotfiles/.emacs.d/init.el
+++ linux/dotfiles/.emacs.d/init.el
@@ -366,6 +366,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+	 (quote
+		("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
  '(package-selected-packages
 	 (quote
 		(lsp-ui company-lsp neotree window-number wgrep use-package solarized-theme google-c-style ggtags company)))
