0ffa54d6d8d3dde29b1ff3b0b67c49d10d5d46d3
kenju <kenjutakano+git@gmail.com>
2020-02-19 04:17:17 +0900

    refine

--- linux/dotfiles/.emacs.d/init.el
+++ linux/dotfiles/.emacs.d/init.el
@@ -331,14 +331,15 @@
 		;; lsp-ui-sideline
 		(lsp-ui-sideline-enable nil)
 		(lsp-ui-sideline-show-hover t)
-		(lsp-ui-sideline-delay 2)
+		(lsp-ui-sideline-delay 0.2)
 
 		;; lsp-ui-peek
 		(lsp-ui-peek-always-show t)
 
 		:custom-face
-		(lsp-ui-sideline-symbol-info ((t (:background "grey10"))))))
-
+		(lsp-ui-sideline-symbol-info ((t (:background "default"))))
+		;; background face of sideline and doc
+		(markdown-code-face ((t (:background "grey10"))))))
 
 ;; google-c-style
 (use-package google-c-style
@@ -404,30 +405,3 @@
 		(setq lsp-clients-python-command '("c:\\python27\\scripts\\pyls.exe"))))
 
 
-;;
-;; customize
-;;
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(clang-format-fallback-style "google" t)
- '(clang-format-style "file" t)
- '(lsp-keymap-prefix "C-q l")
- '(lsp-log-io nil)
- '(lsp-prefer-flymake nil)
- '(lsp-ui-sideline-show-hover t t nil "Customized with use-package lsp-ui")
- '(package-selected-packages
-	 (quote
-		(yasnippet window-number which-key wgrep use-package treemacs tramp projectile neotree mozc lsp-ui ido-vertical-mode ido-completing-read+ google-c-style go-mode ggtags flycheck doom-themes company-lsp clang-format anzu)))
- '(winner-dont-bind-my-keys t))
-
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(lsp-ui-sideline-symbol-info ((t (:background "default")))))
