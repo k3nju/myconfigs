8cc69fb9cae5c489c3c94d3b8331ca2aa2b84800
kenju <kenjutakano+git@gmail.com>
2021-12-07 10:35:32 +0900

    use regex

--- linux/dotfiles/.emacs.d/init.el
+++ linux/dotfiles/.emacs.d/init.el
@@ -108,11 +108,11 @@
 						 (interactive)
 						 (other-window -1)))
 
-;; reverse isearch and regex isearch
+;; use regexp search as default
 (global-set-key (kbd "C-s") 'isearch-forward-regexp)
 (global-set-key (kbd "C-r") 'isearch-backward-regexp)
-(global-set-key (kbd "C-M-s") 'isearch-forward)
-(global-set-key (kbd "C-M-r") 'isearch-backward)
+;;(global-set-key (kbd "C-M-s") 'isearch-forward)
+;;(global-set-key (kbd "C-M-r") 'isearch-backward)
 
 
 ;;
