61c02e1f168f26418d69e75664a54431c80c5768
kenju <kenjutakano+git@gmail.com>
2023-07-04 20:38:11 +0900

    add xlsx

--- linux/dotfiles/reset-ratslap.sh
+++ linux/dotfiles/reset-ratslap.sh
@@ -17,18 +17,25 @@ sudo ratslap \
 		 --rate 250 \
 		 --print f3
 
-# ghidra
-sudo ratslap -m f4 --middle modeswitch
-#sudo ratslap \
-#		 --modify f4 \
-#		 --color red \
-#		 --g4 leftalt+n \
-#		 --g5 leftalt+p \
-#		 --g6 leftalt+f \
-#		 --g7 leftalt+c \
-#		 --print f4
-
 # excel
+#sudo ratslap -m f4 --middle modeswitch
+sudo ratslap \
+		 --modify f4 \
+		 --color green \
+		 --middle modeswitch \
+		 --g4 left \
+		 --g5 right \
+		 --g6 pagedown \
+		 --g7 pageup \
+		 --g8 leftctrl+pageup \
+		 --g9 leftctrl+pagedown \
+		 --d2 2000\
+		 --no-dpishift \
+		 --default-dpi 2 \
+		 --rate 250 \
+		 --print f4
+
+# ghidra
 sudo ratslap -m f5 --middle modeswitch
 #sudo ratslap -m f5 -c green -p f5
 
